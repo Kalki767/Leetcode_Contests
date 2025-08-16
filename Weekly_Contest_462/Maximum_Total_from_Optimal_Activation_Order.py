cdd660bad5c4dbe3e0b05773df5edc8de33f9008
Kalki767 <kalkidankidane3@gmail.com>
2025-08-16 20:49:35 +0300

    Solution for the maximum total from optimal activation order

---
+++ Weekly_Contest_462/Maximum_Total_from_Optimal_Activation_Order.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def maxTotal(self, value: List[int], limit: List[int]) -> int:
+        merged = list(zip(value,limit))
+        merged.sort(key = lambda x : x[1])
+        sorted_values = defaultdict(list)
+        counter = defaultdict(int)
+        for val, lim in merged:
+            sorted_values[lim].append(val)
+        
+        for key in sorted_values:
+            sorted_values[key].sort(reverse = True)
+
+        # print(sorted_values)   
+        total = active = 0
+        for key in sorted_values:
+            for val in sorted_values[key]:
+                if key > active:
+                    active += 1
+                    counter[key] += 1
+                    total += val
+                    # print(active,total)
+                    if key <= active:
+                        active = 0
+                        counter[key] = 0
+                        break
+                    else:
+                        prev_active = active
+                        active -= counter[active]
+                        counter[prev_active] = 0
+                        if active >= key:
+                            break
+                            
+        
+        return total
